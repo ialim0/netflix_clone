371d7582aa5c52121dad46c20baf9eece4c399df
ialim0 <i.alim0229@gmail.com>
2022-07-13 08:41:44 +0000

    Finalisation of previews

--- lib/widgets/previews.dart
+++ lib/widgets/previews.dart
@@ -37,6 +37,7 @@ class Previews extends StatelessWidget {
                     print(content.name);
                   },
                   child: Stack(
+                    alignment: Alignment.center,
                     children: [
                       Container(
                         margin: const EdgeInsets.symmetric(horizontal: 16),
@@ -47,7 +48,43 @@ class Previews extends StatelessWidget {
                             image: AssetImage(
                               content.imageUrl,
                             ),
+                            fit: BoxFit.cover,
                           ),
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: content.color,
+                            width: 4,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 130,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [
+                              Colors.black87,
+                              Colors.black45,
+                              Colors.transparent
+                            ],
+                            stops: [0, 0.25, 1],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: content.color,
+                            width: 4,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        child: SizedBox(
+                          height: 60,
+                          child: Image.asset(content.titleImageUrl),
                         ),
                       )
                     ],
