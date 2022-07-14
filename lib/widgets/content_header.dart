2de50f883b3cda3e65276f7d825aa7aba92e641d
ialim0 <i.alim0229@gmail.com>
2022-07-14 17:41:32 +0000

    Add video player to content header

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/models/models.dart';
 import 'package:flutter_netflix_responsive_ui/widgets/widgets.dart';
+import 'package:video_player/video_player.dart';
 
 class ContentHeader extends StatelessWidget {
   final Content featuredContent;
   const ContentHeader({Key key, @required this.featuredContent})
       : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: _ContentHeaderMobile(featuredContent: featuredContent),
+      desktop: _ContentHeaderDesktop(featuredContent: featuredContent),
+    );
+  }
+}
+
+class _ContentHeaderMobile extends StatelessWidget {
+  final Content featuredContent;
+  const _ContentHeaderMobile({Key key, this.featuredContent}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -64,6 +78,147 @@ class ContentHeader extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class _ContentHeaderDesktop extends StatefulWidget {
+  final Content featuredContent;
+
+  const _ContentHeaderDesktop({Key key, this.featuredContent})
+      : super(key: key);
+
+  @override
+  State<_ContentHeaderDesktop> createState() => _ContentHeaderDesktopState();
+}
+
+class _ContentHeaderDesktopState extends State<_ContentHeaderDesktop> {
+  VideoPlayerController _videoController;
+  bool _isMuted = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _videoController =
+        VideoPlayerController.network(widget.featuredContent.videoUrl)
+          ..initialize().then((_) => setState(
+                () {},
+              ))
+          ..setVolume(0)
+          ..play();
+  }
+
+  @override
+  void dispose() {
+    _videoController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _videoController.value.isPlaying
+          ? _videoController.pause()
+          : _videoController.play(),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          AspectRatio(
+            aspectRatio: _videoController.value.isInitialized
+                ? _videoController.value.aspectRatio
+                : 2.344,
+            child: _videoController.value.isInitialized
+                ? VideoPlayer(_videoController)
+                : Image.asset(
+                    widget.featuredContent.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+          ),
+          Container(
+            height: 500,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.black, Colors.transparent],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topCenter,
+              ),
+            ),
+          ),
+          Positioned(
+            left: 60,
+            right: 60,
+            bottom: 150,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 250,
+                  child: Image.asset(widget.featuredContent.titleImageUrl),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  widget.featuredContent.description,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        offset: Offset(2, 4),
+                        blurRadius: 6,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    _PlayButton(),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.info_outline),
+                      label: const Text(
+                        "More Infos",
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    if (_videoController.value.isInitialized)
+                      IconButton(
+                        icon: Icon(
+                          _isMuted ? Icons.volume_off : Icons.volume_up,
+                        ),
+                        color: Colors.white,
+                        iconSize: 30,
+                        onPressed: () {
+                          setState(
+                            () {
+                              _isMuted
+                                  ? _videoController.setVolume(100)
+                                  : _videoController.setVolume(0);
+                              _isMuted = _videoController.value.volume == 0;
+                            },
+                          );
+                        },
+                      )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
